024d3e2594b600e6f6515c17c5cd8341ac7a540b
Melesar <sergey.pikhota@protonmail.com>
2022-04-11 23:16:02 +0200

    Send camera address to the client

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,12 @@ mod networking;
 
 use std::process::{Command, Stdio};
 use std::convert::TryInto;
-use std::io::Result;
-use std::net::{Ipv4Addr, UdpSocket};
+use std::io::{Result, ErrorKind};
+use std::net::{Ipv4Addr, UdpSocket, SocketAddr};
 use std::time::Duration;
 use std::u128;
 
+use pnet::ipnetwork::IpNetwork;
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::net::TcpSocket;
 
@@ -22,6 +23,8 @@ enum Event {
     Disconnected
 }
 
+const CAMERA_PORT : u16 = 8081;
+
 #[tokio::main]
 async fn main() {
     //Testing code
@@ -34,6 +37,8 @@ async fn main() {
         eprintln!("Camera is not available. Shutting down");
         return;
     }
+    
+    let current_ip = get_current_ip_address().expect("Failed to get current ip address");
 
     //TODO optionally: check for gpio availability
 
@@ -53,20 +58,24 @@ async fn main() {
 
     loop {
         tokio::select! {
-            accept_result = listener.accept() => if let Ok((mut socket, _)) = accept_result {
+            accept_result = listener.accept() => if let Ok((mut stream, _)) = accept_result {
                 let config = config.clone();
                 let sender = tx.clone();
                 tokio::spawn(async move {
-                    let (reader, _writer) = socket.split();
+                    let (reader, mut writer) = stream.split();
                     let mut reader = tokio::io::BufReader::new(reader);
                     let code = reader.read_u128_le().await.unwrap_or(0);
                     println!("Received code {}", code);
                     if code == 0 {
-                        println!("Disconnecting the client");
                         sender.send(Event::Disconnected).await.unwrap_or_default();
                     } else if config.code == code {
-                        println!("Client is connected");
                         sender.send(Event::Connected).await.unwrap_or_default();
+
+                        let mut buffer = [0u8; 6];
+                        buffer[..4].copy_from_slice(&current_ip.octets());
+                        buffer[4..].copy_from_slice(&CAMERA_PORT.to_le_bytes());
+
+                        writer.write(&buffer).await.unwrap_or_default();
                     }
                 });
             },
@@ -108,6 +117,24 @@ fn disable_camera() {
 
 }
 
+fn get_current_ip_address() -> Result<Ipv4Addr> {
+    let err = || std::io::Error::new(std::io::ErrorKind::NotFound, "No network interfaces found");
+
+    let interface = pnet::datalink::interfaces()
+        .into_iter()
+        .filter(|i| i.is_up() && !i.is_loopback() && !i.ips.is_empty())
+        .take(1)
+        .next()
+        .ok_or_else(err)?;
+
+    interface.ips.first()
+        .and_then(|addr| match addr {
+            IpNetwork::V4(ipv4_addr) => Some(ipv4_addr.ip()),
+            _ => None,
+        })
+        .ok_or_else(err)
+}
+
 fn check_camera_availability() -> bool {
     Command::new("motion")
         .arg("-h")
@@ -140,6 +167,16 @@ async fn start_client() -> Result<()> {
 
     let mut tcp_stream = TcpSocket::new_v4()?.connect(sender).await?;
     tcp_stream.write_all_buf(&mut code).await?;
+
+    let mut buffer = vec![];
+    tcp_stream.read_to_end(&mut buffer).await?;
+
+    let address = Ipv4Addr::new(buffer[0], buffer[1], buffer[2], buffer[3]);
+    let mut port_bytes = [0u8; 2];
+    port_bytes.copy_from_slice(&buffer[4..]);
+    let port = u16::from_le_bytes(port_bytes);
+
+    println!("Received address {}:{}", address, port);
 
     Ok(())
 }
--- src/networking.rs
+++ src/networking.rs
@@ -1,11 +1,15 @@
-use std::net::{UdpSocket, Ipv4Addr};
 use std::io::Result;
+use std::io::Write;
+use std::net::{Ipv4Addr, UdpSocket};
 use std::u128;
+use tokio::io::{AsyncWrite, AsyncWriteExt};
+use tokio::net::TcpStream;
+
 use crate::ServerConfig;
 
-static ANY_ADDR : Ipv4Addr = Ipv4Addr::new(0, 0, 0, 0);
-static MULTICAST_ADDRESS : Ipv4Addr = Ipv4Addr::new(239, 255, 6, 6);
-static MULTICAST_PORT : u16 = 6688;
+static ANY_ADDR: Ipv4Addr = Ipv4Addr::new(0, 0, 0, 0);
+static MULTICAST_ADDRESS: Ipv4Addr = Ipv4Addr::new(239, 255, 6, 6);
+static MULTICAST_PORT: u16 = 6688;
 
 pub fn create_server_multicast_socket() -> Result<UdpSocket> {
     UdpSocket::bind((ANY_ADDR, MULTICAST_PORT))
@@ -18,13 +22,13 @@ pub fn send_multicast_packet(socket: &UdpSocket, config: &ServerConfig) -> Resul
     let mut buf = [0u8; 1000];
     let mut ptr = 0;
 
-    buf[ptr..ptr+4].copy_from_slice(&(code_length + name_length).to_le_bytes());
+    buf[ptr..ptr + 4].copy_from_slice(&(code_length + name_length).to_le_bytes());
     ptr += 4;
 
-    buf[ptr..ptr+code_length].copy_from_slice(&config.code.to_le_bytes());
+    buf[ptr..ptr + code_length].copy_from_slice(&config.code.to_le_bytes());
     ptr += code_length;
 
-    buf[ptr..ptr+name_length].copy_from_slice(config.display_name.as_bytes());
+    buf[ptr..ptr + name_length].copy_from_slice(config.display_name.as_bytes());
     ptr += name_length;
 
     socket.send_to(&buf[0..ptr], (MULTICAST_ADDRESS, MULTICAST_PORT))?;
