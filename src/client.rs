9b06eaa42784948924187420ce8bfb81acd4a807
Melesar <sergey.pikhota@protonmail.com>
2022-06-12 20:45:56 +0200

    Finalize messaging prototype

--- src/client.rs
+++ src/client.rs
@@ -1,32 +1,32 @@
 use messages::HelloRequest;
 use prost::Message;
-use tokio::{net::TcpSocket, io::AsyncWriteExt};
+use tokio::{net::TcpSocket, io::{AsyncWriteExt, AsyncReadExt}};
 
 pub mod messages {
     include!(concat!(env!("OUT_DIR"), "/messages.rs"));
 }
 
 
-
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
     let sender = "0.0.0.0:6688".parse().unwrap();
     let mut tcp_stream = TcpSocket::new_v4()?.connect(sender).await?;
-    for _ in [0..2] {
-        tcp_stream.write_u32(0).await?;
+    tcp_stream.write_u32(0).await?;
 
-        let mut message = HelloRequest::default();
-        message.code = 123;
-        tcp_stream.write_u32(message.encoded_len() as u32).await?;
+    let mut message = HelloRequest::default();
+    message.code = 123;
+    tcp_stream.write_u32(message.encoded_len() as u32).await?;
 
-        let mut buffer = Vec::new();
-        match message.encode(&mut buffer) {
-            Ok(_) => {
-                tcp_stream.write_all(&mut buffer).await?;
-            },
-            Err(e) => eprintln!("Failed to encode a message {}", e),
-        }
+    let mut buffer = Vec::new();
+    match message.encode(&mut buffer) {
+        Ok(_) => {
+            tcp_stream.write_all(&mut buffer).await?;
+        },
+        Err(e) => eprintln!("Failed to encode a message {}", e),
     }
+
+    let number = tcp_stream.read_u32().await?;
+    println!("Got back a number: {}", number);
 
     Ok(())
 }
--- src/test.rs
+++ src/test.rs
@@ -1,13 +1,14 @@
 use prost::{bytes::{Buf, BufMut}, DecodeError};
 use messages::{HelloRequest, HelloResponse};
 use prost::Message;
-use tokio::io::AsyncReadExt;
+use tokio::io::{AsyncReadExt, AsyncRead, AsyncWriteExt};
+use tokio::sync::mpsc;
 
 pub mod messages {
     include!(concat!(env!("OUT_DIR"), "/messages.rs"));
 }
 
-#[derive(Clone, Copy)]
+#[derive(Clone, Copy, Debug)]
 enum MessageType {
     HelloRequest,
     HelloResponse
@@ -19,11 +20,46 @@ enum ReadState {
     Payload
 }
 
+struct ReceivedMessage {
+    sender_id: u32,
+    msg_type: MessageType,
+    payload: Vec<u8>
+}
+
+unsafe impl Send for ReceivedMessage {}
+
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
     let listener = tokio::net::TcpListener::bind("0.0.0.0:6688").await.unwrap();
-    let (mut stream, _sender) = listener.accept().await?;
-    let (reader, writer) = stream.split();
+    let (stream, _sender) = listener.accept().await?;
+    let (reader, mut writer) = stream.into_split();
+    let (tx, mut rx) = mpsc::channel(10);
+    tokio::spawn(async move { handle_connection(reader, tx.clone()).await });
+
+    if let Some(message) = rx.recv().await {
+        match message.msg_type {
+            MessageType::HelloRequest => {
+                let mut cursor = std::io::Cursor::new(message.payload);
+                match HelloRequest::decode(&mut cursor) {
+                    Ok(hello_request) => {
+                        println!("Received hello request with code {}", hello_request.code);
+                        writer.write_u32(42).await?;
+                    },
+                    Err(decode_error) => {
+                        return Err(std::io::Error::new(std::io::ErrorKind::Other, decode_error));
+                    }
+                }
+            },
+            MessageType::HelloResponse => todo!(),
+        }
+    }
+
+    Ok(())
+}
+
+async fn handle_connection<R>(reader: R, sender: mpsc::Sender<ReceivedMessage>) -> Result<(), std::io::Error>
+    where R: AsyncRead + std::marker::Unpin {
+
     let mut reader = tokio::io::BufReader::new(reader);
 
     let messages_lookup = vec![MessageType::HelloRequest, MessageType::HelloResponse];
@@ -33,6 +69,7 @@ async fn main() -> Result<(), std::io::Error> {
     let mut buffer : Vec<u8> = Vec::with_capacity(2048);
     let mut current_message_type = MessageType::HelloRequest;
     let mut message_length = 0;
+
     loop {
         match current_state {
             ReadState::MsgType => {
@@ -62,20 +99,13 @@ async fn main() -> Result<(), std::io::Error> {
                 let bytes_read = reader.read(&mut buffer[0..message_length]).await?;
                 if bytes_read < buffer.len() { continue; }
 
-                match current_message_type {
-                    MessageType::HelloRequest => {
-                        let mut cursor = std::io::Cursor::new(buffer);
-                        match HelloRequest::decode(&mut cursor) {
-                            Ok(request) => {
-                                println!("Received request {:?}", request);
-                                current_state = ReadState::MsgType;
-                                buffer = Vec::with_capacity(2048);
-                            }
-                            Err(e) => { eprintln!("Failed to decode a message {}", e); break },
-                        }
-                    },
-                    MessageType::HelloResponse => todo!(),
-                }
+                sender.send(ReceivedMessage {
+                    sender_id: 1,
+                    msg_type: current_message_type,
+                    payload: buffer[0..message_length].to_owned() 
+                })
+                .await
+                .map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, e.to_string()))?;
             },
         }
     }
